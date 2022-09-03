18aa9760ff9e65e39e8ba1d93f44e83820d91115
Ismael Carvajal <ismaelcarvajal@outlook.com>
2022-09-02 22:49:35 -0500

    Updates

---
+++ precache-manifest.9ed1adabc5e2c9bce56899ff51aabec8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cd6e8ecaa0c41d64321",
+    "url": "/entregable-frontend3/static/css/main.18e9269c.chunk.css"
+  },
+  {
+    "revision": "0cd6e8ecaa0c41d64321",
+    "url": "/entregable-frontend3/static/js/main.0cd6e8ec.chunk.js"
+  },
+  {
+    "revision": "ff2aeb385757ea353d47",
+    "url": "/entregable-frontend3/static/js/1.ff2aeb38.chunk.js"
+  },
+  {
+    "revision": "ab68ed8273b609c632ae",
+    "url": "/entregable-frontend3/static/js/runtime~main.ab68ed82.js"
+  },
+  {
+    "revision": "70cf6a09a9b9218682a03cb25976b127",
+    "url": "/entregable-frontend3/index.html"
+  }
+];
